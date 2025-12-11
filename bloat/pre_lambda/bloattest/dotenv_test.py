02d91761623a910608cda5cff6c84b33a9b0f66e
Jerrold Koh <jerroldfisher@live.com>
2025-12-11 23:27:06 +0800

    Deleted bloat folder

--- bloat/pre_lambda/bloattest/dotenv_test.py
+++
@@ -1,9 +0,0 @@
-import os
-from os.path import join, dirname
-from dotenv import load_dotenv
-
-def get_endpoint():
-    dotenv_path = join(dirname(__file__), '.env')
-    load_dotenv(dotenv_path)
-
-    return os.environ.get("SAGE_TS_ENDPOINT")
